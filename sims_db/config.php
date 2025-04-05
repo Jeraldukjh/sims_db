fb72bacf3edd38c7a9922a4e554e437b53c99188
Maca, Jerald Jhon F. <119981870+Jeraldukjh@users.noreply.github.com>
2025-04-05 14:10:51 +0800

    Delete sims_db directory

--- sims_db/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','','sims_db');
-
-?>
--- sims_db/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION = []; // Clear all session variables
-session_unset(); // Unset session data
-session_destroy(); // Destroy session
-header("Location: Home.php");
-exit();
-?>
